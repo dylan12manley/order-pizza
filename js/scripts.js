388ac62464b49ee1c4f758d7c6d76908b524cea5
Dylan Manley <dylan.manley@gmail.com>
2019-11-10 22:21:56 -0800

    get functionality in JS except for the for each loop in the getprice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ function Pizza(size, toppings, price) {
 }
 
 Pizza.prototype.getPrice = function() {
+
+  this.toppings = $(".toppingInput:checked").get();
+  console.log(this.toppings);
+  this.toppings.forEach(function(topping) {
+    this.price += 2;    
+  });
+  console.log(this.price);
   if (this.size === "XL"){
     this.price += 24;
   } else if (this.size === "Large"){
@@ -15,9 +22,6 @@ Pizza.prototype.getPrice = function() {
   } else if (this.size === "Small"){
     this.price += 12;
   }
-  this.toppings.forEach(function(toppings) {
-    this.price += 2;
-  });
   return this.price
 }
  // User Interface//
@@ -25,15 +29,14 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
     let toppingsInputs = $(".toppingInput:checked").get();
-    console.log(toppingsInputs);
     let toppingsArray = [];
     let sizeInput = $("select#sizes").val();
-    console.log(sizeInput);
     toppingsInputs.forEach(function(topping) {
       toppingsArray.push(topping.value);
     });
-    let myPizza = new Pizza(sizeInput, toppingsArray, 0)
+    let myPizza = new Pizza(sizeInput, toppingsArray);
     let price = myPizza.getPrice();
+    console.log(price);
     $('#pizzaResults').fadeOut(function() {
       $(this).text("You have ordered one "+myPizza.size+" with "+toppingsArray+" for the price of $"+price).fadeIn();
     });
